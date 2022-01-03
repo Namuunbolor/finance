10919132fda7e4d69c175e6972c10036ba6b05e4
namuunaa <namunbolor@gmail.com>
2022-01-03 19:10:26 +0800

    ugugdul delgetsees olj awah

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: "add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 var financeController = (function () {})();
 
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function () {
-    console.log("Дэлгэцээс өгөдлөө авах хэсэг");
-  };
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
     // oruulah ugugdul delgetsees olj awna.
+    console.log(uiController.getInput());
     // olj awsan ugugdluudee sanhuud hadgalna.
     // olj awsan ugugdluudee web deeree tohiroh hesegt gargana.
     // Tusviig tootsoolno
     // etssiin uldegdel tootsoog gargana.
+  };
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
+    ctrlAddItem();
   });
 
   document.addEventListener("keypress", function (event) {
